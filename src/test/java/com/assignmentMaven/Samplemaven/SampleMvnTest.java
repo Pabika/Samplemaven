ddede1206c9a369894a604ca2da0a1097abb1b1c
Pabika Rai <Pabika@leadsquared.com>
2020-03-15 10:45:13 +0530

    initial commit

---
+++ src/test/java/com/assignmentMaven/Samplemaven/SampleMvnTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.assignmentMaven.Samplemaven;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author Pabika
+ *
+ */
+public class SampleMvnTest {
+
+	@Test
+	public void sample()
+	{
+		System.out.println("Yo");
+	}
+}
